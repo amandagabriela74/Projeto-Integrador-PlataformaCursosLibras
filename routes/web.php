7971f723b78c82454c8272f346e30549214dc852
Gleydson José <gleydson_jose333@outlook.com>
2022-11-10 03:22:53 -0300

    Modificando rota da pagina inicial

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view(' index');
 });
 
 //a rota jogos, chama o controller CursosController, na puclic function index
