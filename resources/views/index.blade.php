c0eeefb0fd5272b10524bc694e371a290cca12fa
Gleydson José <gleydson_jose333@outlook.com>
2022-11-23 01:04:18 -0300

    altereção do responsivo do index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,16 +15,14 @@
             @endif
         </header>
 
-        <section class="flex mt-40 px-20">
-            <div>
-
-                <h2 class="text-5xl text-white w-1/2">Torne-se um apaixonado por linguagem de sinais</h2>
-                
-                <a class="inline-block mt-5 rounded-xl text-white bg-blue-600 p-2 px-14" href="">Embarque</a>
-            </div>
-            <div class="bg-slate-100 w-80 h-60 rounded-xl">AQUI TERA UM CARD
-
-
+        <section>
+            <div class="max-w-screen-2xl h-[500px] mx-auto sm:px-6 lg:px-20 flex justify-center items-center">
+                <div>
+                    <h2 class="text-5xl text-white w-1/2">Torne-se um apaixonado por linguagem de sinais</h2>
+                    
+                    <a class="inline-block mt-5 rounded-xl text-white bg-[#21DF40] p-2 px-14" href="">Embarque</a>
+                </div>
+                <div class="bg-slate-100 w-80 h-60 rounded-xl">AQUI TERA UM CARD</div>
             </div>
         </section>
     </div>
