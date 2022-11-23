5df25ee26f2032eb208384858dbdfb4eb0922728
1thomas-santos <thomasbez11@gmail.com>
2022-11-23 00:50:52 -0300

    alteração no controller de registeruser

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,7 +43,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'is_admin'=>$request->is_admin
+            'is_admin'=>false, 
         ]);
 
         event(new Registered($user));
