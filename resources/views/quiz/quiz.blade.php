93d99ca0c5eae16fcbc56e977d8a0d411f19aee0
amandagabriela74 <amandagabriela056@gmail.com>
2022-12-03 15:26:12 -0300

    novas alterações no quiz - front

--- resources/views/quiz/quiz.blade.php
+++ resources/views/quiz/quiz.blade.php
@@ -7,25 +7,25 @@
             <h1 class="flex justify-center py-2">Quiz: {{ $quiz->title }}</h1>
             
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center ">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-3/5  ">
-                    <div class="p-6 bg-white border-b border-gray-200 ">
-                        <section class="flex flex-col justify-center items-center ">
+                <div class=" overflow-hidden shadow-sm sm:rounded-lg w-3/5  ">
+                    <div class="p-6 ">
+                        <section class="flex flex-col justify-center items-center  ">
                             <form method="POST" action="{{ route('test-store') }}" class="w-full ">
                                 @csrf
                                 <input type="hidden" name="quizId" value="{{ $quiz->id }}">
-                                <section id="" class="w-9/12 text-center">
+                                <section id="" class=" text-center">
                                     <div>
                                         @foreach ($quiz->questions as $question)
-                                            <div>
-                                                <div class=" rounded-md">
+                                            <div class=" bg-white mb-2 rounded-xl">
+                                                <div class=" rounded-md py-8">
                                                     <h1 id="" class="py-2 text-2xl font-semibold">
                                                         {{ $question->question }}</h1>  <!--pergunta-->
                                                 </div>
-                                                <div class="pb-5">
+                                                <div class="pb-5 flex flex-col items-center ">
 
                                                     <!-- Module::find(1)-> quizzes[0]->questions[0]->alternatives -->
                                                     @foreach ($question->alternatives as $alternative)
-                                                        <div class="flex flex-row border border-black border-solid py-2 my-2 rounded-xl ">
+                                                        <div class="flex flex-row border border-blue-500 border-solid py-2 my-2 rounded-xl w-4/5 hover:bg-sky-200 cursor-pointer">
                                                             <input class="ml-2"
                                                                 id="alternative-{{ $alternative->id }}"
                                                                 type="radio" name="questions[{{ $question->id }}]"
@@ -36,18 +36,22 @@
                                                             </label>
                                                         </div>
                                                     @endforeach
+                                                    <div class="">
+                                                        <x-text-input type="submit" name="submit" value="Enviar"
+                                                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded cursor-pointer w-24 mb-4 mr-4" />
+                                                    </div>
                                                 </div>
                                             </div>
                                         @endforeach
                                     </div>
                                 </section>
         @endforeach
-        <div class="flex flex-col">
+        {{-- <div class="flex flex-col">
             <x-text-input type="submit" name="submit" value="Enviar"
                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded cursor-pointer w-24 mt-4" />
-        </div>
-        <h2 id="">Resultado:</h2>
-        <h2 id="">Pontos:</h2>
+        </div> --}}
+        {{-- <h2 id="">Resultado:</h2>
+        <h2 id="">Pontos:</h2> --}}
 
         </form>
 
