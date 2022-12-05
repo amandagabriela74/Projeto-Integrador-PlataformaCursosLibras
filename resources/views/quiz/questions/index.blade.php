de1e54befc681b210844151978945b31e87bea69
amandagabriela74 <amandagabriela056@gmail.com>
2022-12-05 01:56:32 -0300

    Organizando front do painel admin: quiz

--- resources/views/quiz/questions/index.blade.php
+++ resources/views/quiz/questions/index.blade.php
@@ -1,15 +1,6 @@
 <x-app-layout>
-    <x-slot name="header">
-
-        <div>
-            <a href="{{ route('questions.create') }}"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded cursor-pointer w-32 ">
-                Adicionar nova questão</a>
-        </div>
-
-    </x-slot>
     @if (Auth::user()->is_admin)
-        <section class="flex justify-center ">
+        <section class="flex justify-center">
         <x-menu></x-menu>
             <div x-show="open" class="w-3/4 flex flex-row justify-center gap-2">
                 fvrsdfv
