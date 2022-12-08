f2f08531fe820f52776b46f35a0022cbdb0d4184
Gleydson José <77823704+Gjss333@users.noreply.github.com>
2022-12-08 02:18:05 -0300

    restruturando tela de login e cadastro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
         </x-slot>
         
         <!-- Session Status -->
-        <x-auth-session-status class="-mt-14" :status="session('status')" />  
+        <x-auth-session-status class="mb-4" :status="session('status')" />  
         
         <form method="POST" action="{{ route('login') }}">
             @csrf
@@ -16,7 +16,7 @@
             <div>
                 <x-input-label for="email" :value="__('Email')" />
 
-                <input id="email" class="mb-8 bg-transparent block mt-1 w-full border-0 border-blue-400 border-b-2 border-blue-400 appearance-none rounded-none relative block w-full px-3 py-2 placeholder-blue-400 text-blue-400 rounded-sm text-xl focus:outline-none focus:ring-blue-500 focus:border-opacity-0" type="email" name="email" :value="old('email')" placeholder="E-mail" required autofocus />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
 
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
@@ -25,40 +25,21 @@
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
 
-                <input id="password" class="mb-8 bg-transparent block mt-1 w-full border-0 border-blue-400 border-b-2 border-blue-400 appearance-none rounded-none relative block w-full px-3 py-2 placeholder-blue-400 text-blue-400 rounded-sm text-xl focus:outline-none focus:ring-blue-500 focus:border-opacity-0" name="password" type="password" autocomplete="current-password" required autocomplete="current-password" required class="block mt-1 w-full"  placeholder="Senha"/>
+                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+
 
             </div>
             
-                                    </div>          
-
-                                    <div class="flex items-center justify-between">
-                                        <div class="flex items-center">
-                                          <input id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 bg-transparent text-red-400 focus:ring-blue-500 border-blue-400 rounded">
-                                          <label for="remember_me" class="ml-2 block text-sm text-blue-400">
-                                             <span>{{ __('Lembre-me') }}</span>
-                                         </label>
-
-                                         </div>  
-                                        <div class="text-sm">
-                                          @if (Route::has('password.request'))
-                                            <a class="underline text-sm text-blue-400 hover:text-white" href="{{ route('password.request') }}">
-                                            {{ __('Esqueceu a senha?') }}
-                                            </a>
-                                          @endif
-                                        </div>
-                                        </div>          
-
-<div>
-    <button type="submit" class="mb-12 shadow-2xl group relative w-full flex justify-center py-2 px-4 border border-transparent text-xl font-medium rounded-md text-blue-900 bg-blue-400 hover:bg-white hover:text-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-      <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-        <!-- Heroicon name: solid/lock-closed -->
-        <svg class="h-5 w-5 text-blue-900 group-hover:text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-        </svg>
-      </span>
-        {{ __('Entrar') }}
-    </button>
-</div>      
+            <!-- Remember Me -->
+            <div class="block mt-4">
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+     
 
             <div class="flex items-center justify-center mt-4">
                 <x-primary-button class="ml-3">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,8 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <!-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> -->
-                <img src="/images/101.jpg" alt="">
+                <h1>Libras</h1> 
             </a>
         </x-slot>
 
@@ -14,8 +13,7 @@
             <div>
                 <x-input-label for="name" :value="__('Name')" />
 
-                <x-text-input id="name" class="mb-8 bg-transparent block mt-1 w-full border-0 border-blue-400 border-b-2 border-blue-400 appearance-none rounded-none relative block w-full px-3 py-2 placeholder-blue-400 text-blue-400 rounded-sm text-xl focus:outline-none focus:ring-blue-500 focus:border-opacity-0" type="name" name="name" :value="old('name')" placeholder="name" required autofocus />
-
+                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
 
                 <x-input-error :messages="$errors->get('name')" class="mt-2" />
             </div>
@@ -24,8 +22,7 @@
             <div class="mt-4">
                 <x-input-label for="email" :value="__('Email')" />
 
-                <x-text-input id="email" class="mb-8 bg-transparent block mt-1 w-full border-0 border-blue-400 border-b-2 border-blue-400 appearance-none rounded-none relative block w-full px-3 py-2 placeholder-blue-400 text-blue-400 rounded-sm text-xl focus:outline-none focus:ring-blue-500 focus:border-opacity-0" type="email" name="email" :value="old('email')" placeholder="E-mail" required autofocus />
-
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
 
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
@@ -34,7 +31,7 @@
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
 
-                <x-text-input id="Password" class="mb-8 bg-transparent block mt-1 w-full border-0 border-blue-400 border-b-2 border-blue-400 appearance-none rounded-none relative block w-full px-3 py-2 placeholder-blue-400 text-blue-400 rounded-sm text-xl focus:outline-none focus:ring-blue-500 focus:border-opacity-0" type="Password" name="Password" :value="old('Password')" placeholder="Password" required autofocus />
+                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
 
 
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
@@ -44,7 +41,7 @@
             <div class="mt-4">
                 <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                <x-text-input id="Password" class="mb-8 bg-transparent block mt-1 w-full border-0 border-blue-400 border-b-2 border-blue-400 appearance-none rounded-none relative block w-full px-3 py-2 placeholder-blue-400 text-blue-400 rounded-sm text-xl focus:outline-none focus:ring-blue-500 focus:border-opacity-0" type="Password" name="Password" :value="old('Password')" placeholder="Password" required autofocus />
+                <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required />
             
 
                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
@@ -55,34 +52,10 @@
                     {{ __('Already registered?') }}
                 </a>
 
-                <x-primary-button class="ml-4 justify-center text-white bg-blue-900 hover:bg-blue-400 hover:text-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-            {{ __('Finalizar') }}
+                <x-primary-button class="ml-4">
+                    {{ __('Register') }}
                 </x-primary-button>
             </div>
         </form>
-        </div>
-</div>
-</div>
-</div>
-</div>
-</div>
-
-<div class="bg-blue-400">
-  <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-8 lg:px-8 lg:flex lg:items-center lg:justify-between">
-    <h2 class="text-2xl tracking-tight text-white sm:text-lg">
-      <span class="block"> © 2022 !Perfect Codes - PlataformaCursosLibras- Todos os direitos reservados</span>
-    </h2>
-    <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-      <div class="ml-3 inline-flex rounded-full h-10 w-10 flex items-center justify-center shadow-none">
-        <a href="https://github.com/amandagabriela74/Projeto-Integrador-PlataformaCursosLibras">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="text-xl text-white hover:text-blue-900 dark:hover:text-white transition-colors duration-200" viewBox="0 0 1792 1792">
-            <path d="M896 128q209 0 385.5 103t279.5 279.5 103 385.5q0 251-146.5 451.5t-378.5 277.5q-27 5-40-7t-13-30q0-3 .5-76.5t.5-134.5q0-97-52-142 57-6 102.5-18t94-39 81-66.5 53-105 20.5-150.5q0-119-79-206 37-91-8-204-28-9-81 11t-92 44l-38 24q-93-26-192-26t-192 26q-16-11-42.5-27t-83.5-38.5-85-13.5q-45 113-8 204-79 87-79 206 0 85 20.5 150t52.5 105 80.5 67 94 39 102.5 18q-39 36-49 103-21 10-45 15t-57 5-65.5-21.5-55.5-62.5q-19-32-48.5-52t-49.5-24l-20-3q-21 0-29 4.5t-5 11.5 9 14 13 12l7 5q22 10 43.5 38t31.5 51l10 23q13 38 44 61.5t67 30 69.5 7 55.5-3.5l23-4q0 38 .5 88.5t.5 54.5q0 18-13 30t-40 7q-232-77-378.5-277.5t-146.5-451.5q0-209 103-385.5t279.5-279.5 385.5-103zm-477 1103q3-7-7-12-10-3-13 2-3 7 7 12 9 6 13-2zm31 34q7-5-2-16-10-9-16-3-7 5 2 16 10 10 16 3zm30 45q9-7 0-19-8-13-17-6-9 5 0 18t17 7zm42 42q8-8-4-19-12-12-20-3-9 8 4 19 12 12 20 3zm57 25q3-11-13-16-15-4-19 7t13 15q15 6 19-6zm63 5q0-13-17-11-16 0-16 11 0 13 17 11 16 0 16-11zm58-10q-2-11-18-9-16 3-14 15t18 8 14-14z">
-            </path>
-          </svg>
-        </a>
-      </div>
-    </div>
-  </div>
-</div>
     </x-auth-card>
 </x-guest-layout>
