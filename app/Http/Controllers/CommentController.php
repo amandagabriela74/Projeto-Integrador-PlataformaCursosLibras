e4cfd925364ee5f3bd5b372bd700800fdc265d33
amandagabriela74 <amandagabriela056@gmail.com>
2023-01-17 16:25:27 -0300

    criação do Model Comments (de comentários)

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Course;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //dd($request);
+        $validated = $request->validate([
+            'comment' => 'required'
+        ]);
+        //dd($validated);
+
+        $created = Comment::create([
+            'comment'=>$validated['comment'],
+            'user_id'=>auth()->user()->id,
+            'course_id'=> $request->input('courseId')
+        ]);
+
+        //dd($created);
+        return back();
+
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+       //dd($id);
+       Comment::where('id',$id)->delete();
+       return back();
+    }
+}
+
