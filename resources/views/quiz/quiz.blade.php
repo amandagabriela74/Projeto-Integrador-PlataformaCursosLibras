22a9f1c9da28a43297a1e29abd8f2b75e52148ae
amandagabriela74 <amandagabriela056@gmail.com>
2023-01-18 15:33:11 -0300

    .

--- resources/views/quiz/quiz.blade.php
+++ resources/views/quiz/quiz.blade.php
@@ -1,42 +1,30 @@
 <x-app-layout>
-    <x-slot name="header">
 
-    </x-slot>
-    <div class="py-12 bg-sky-400">
-        <h1 class="flex justify-center py-2">Quiz: {{ $quiz->title }}</h1>
+
+    <a href="{{route('dashboard')}}" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+        Dashboard
+        <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </a>
+    
+    <div class="py-12 bg-sky-400 ">
+        <h2 class="flex justify-center py-2 text-4xl font-extrabold dark:text-white">Quiz: {{ $quiz->title }}</h2>
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center ">
             <div class="overflow-hidden shadow-sm sm:rounded-lg w-3/5  ">
                 <div class="p-6 ">
                     <section class="flex flex-col justify-center items-center  ">
-                        <form method="POST" action="{{ route('test-store') }}" class="w-full " x-data="{question: 0}">
+                        <form method="POST" action="{{ route('test-store') }}" class="w-full " x-data="{ question: 0 }">
                             @csrf
                             <input type="hidden" name="quizId" value="{{ $quiz->id }}">
                             <section id="" class=" text-center">
                                 <div>
                                     @foreach ($quiz->questions as $i => $question)
-                                    
-                                  <x-quiz-question :i="$i" :question="$question" :quiz="$quiz" />
-                                    
+                                        <x-quiz-question :i="$i" :question="$question" :quiz="$quiz" />
                                     @endforeach
                                 </div>
                             </section>
-
-
-
-
-                            {{-- <div class="flex flex-col">
-            <x-text-input type="submit" name="submit" value="Enviar"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded cursor-pointer w-24 mt-4" />
-        </div> --}}
-                            {{-- <h2 id="">Resultado:</h2>
-        <h2 id="">Pontos:</h2> --}}
-
                         </form>
-
                     </section>
-
-
                 </div>
             </div>
         </div>
