cf30dcdd7a811485304a089d57fa4b47f3edea19
amandagabriela74 <amandagabriela056@gmail.com>
2023-01-18 15:32:41 -0300

    organizando views do quiz

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -1,5 +1,10 @@
 <x-app-layout class="">
 
+    <a href="{{route('dashboard')}}" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+        Dashboard
+        <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </a>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center ">
             <div class=" overflow-hidden shadow-sm sm:rounded-lg w-3/5  bg-white ">
