a85f8845221902df8ab562728adc2d365dd2e94c
amandagabriela74 <amandagabriela056@gmail.com>
2023-01-20 02:45:32 -0300

    editando texto errado

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -12,7 +12,7 @@
 
     </div>
     <div class="bg-sky-400 w-5/12 h-scren text-center flex flex-col justify-center items-center">
-        <h1 class="text-white mb-20 text-5xl p-3">Libras Intergrador</h1>
+        <h1 class="text-white mb-20 text-5xl p-3">Libras Integrador</h1>
         <p class="text-white p-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium voluptatum atque recusandae aut, fuga amet repel</p>
         
         <a class="text-sky-400 m-2 px-3 py-2 bg-white font-extrabold drop-shadow-xl rounded-xl" href="{{ route('register') }}">Cadastrar-se</a>
