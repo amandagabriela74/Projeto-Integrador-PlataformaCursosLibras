2c49c4e88343d0db4c7adbf309b0a902148c6e69
amandagabriela74 <amandagabriela056@gmail.com>
2023-01-22 23:47:24 -0300

    alterando certificado

---
+++ resources/views/users/certificate.blade.php
@@ -0,0 +1,6 @@
+<br><br><br><br><br><br>
+<p>Certificamos que</p>
+<h1>Amanda</h1>
+<p>em janeiro de  12/34/2021, concluiu o curso</p>
+<h1>Alfabeto em Libras</h1>
+<p>No formato curso online livre, com carga orária de 3hrs</p>
