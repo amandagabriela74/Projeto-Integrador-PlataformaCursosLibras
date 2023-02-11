51c2e3044450707023b82d9af6433a29ed482685
amandagabriela74 <amandagabriela056@gmail.com>
2023-02-11 15:23:42 -0300

    melhorando a estilização da aba dos cursos

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -11,48 +11,58 @@ use Illuminate\Http\Request;
 class CourseController extends Controller
 {
 
-    public function create(){
-      $modules = Module::all();
-      return view('courses.create', compact('modules'));
-    }
+  public function create()
+  {
+    $modules = Module::all();
+    return view('courses.create', compact('modules'));
+  }
 
 
-    public function store(Request $request){
-      Course::create([
-        'titulo' => $request->titulo,
-        'topico' => $request->topico,
-        'embed' => $request->embed,
-        'descricao' => $request->descricao,
-        'module_id' => $request->moduleId
-      ]);
-  
-      return redirect()->route('modules.index');
+  public function store(Request $request)
+  {
+    Course::create([
+      'titulo' => $request->titulo,
+      'topico' => $request->topico,
+      'embed' => $request->embed,
+      'descricao' => $request->descricao,
+      'module_id' => $request->moduleId
+    ]);
 
-    }
+    return redirect()->route('modules.index');
+  }
 
-    public function edit($id){
-     
+  public function show(Module $module)
+  {
+    $user = auth()->user();
+    $courses = $module->courses;
+    return view('course', ['moduleCourses' => $courses, 'user' => $user]);
+  }
+
+  public function edit($id)
+  {
+
     $course = Course::where('id', $id)->first();
-    
-    return view('courses.edit',compact('course'));
-    }
+    return view('courses.edit', compact('course'));
+  }
 
-    public function update(Request $request, Course $course){
+  public function update(Request $request, Course $course)
+  {
 
-      $data = [
-        'titulo'=> $request->titulo,
-        'topico'=> $request->topico,
-        'embed'=> $request->embed,
-        'descricao'=> $request->descricao,        
-      ];
+    $data = [
+      'titulo' => $request->titulo,
+      'topico' => $request->topico,
+      'embed' => $request->embed,
+      'descricao' => $request->descricao,
+    ];
 
-      $course->update($data);
-      return redirect()->route('modules.index');
-    }
+    $course->update($data);
+    return redirect()->route('modules.index');
+  }
 
-    public function destroy($id){
-      //dd($id);
-      Course::where('id',$id)->delete();
-      return back();
-    }
+  public function destroy($id)
+  {
+
+    Course::where('id', $id)->delete();
+    return back();
+  }
 }
--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -1,11 +1,7 @@
 <x-app-layout>
-    {{--   <x-slot name=header>
-
-    </x-slot>
-     --}}
     <section x-data="{ open: {{ $moduleCourses[0]->id ?? 'false' }} }" class="container-fluid flex flex-row">
         <div
-            class="w-2/6 h-screen flex flex-col justify-start items-center pt-6 overflow-y-auto rounded text-neutral-50 bg-sky-500">
+            class="w-96 h-screen flex flex-col justify-start items-center pt-6 overflow-y-auto rounded text-neutral-50 bg-sky-500">
             <div class=" h-16 overflow-hidden shadow-sm ">
                 <div class=" border-gray-200">
                     <a href="{{ route('dashboard') }}"
@@ -25,16 +21,13 @@
                     <li>
                         <div x-on:click="open = {{ $courses->id }}"
                             class=" h-16  w-screen bg-black/20  hover:bg-blue-500 text-center rounded-md cursor-pointer border-b-2">
-                            <h4 class="text-2xl font-bold dark:text-white">{{ $courses->titulo }}</h4>
+                            <h4 class="pt-4 text-2xl text-center font-bold dark:text-white">{{ $courses->titulo }}</h4>
                         </div>
                     </li>
                 @endforeach
 
             </ul>
         </div>
-
-
-
         <div class="h-screen w-full">
             <div class="w-full">
 
@@ -44,111 +37,95 @@
                         class=" h-screen flex flex-row justify-center   bg-gray-50 ">
                         <div class=" flex flex-col spcace-y-4 items-center  w-full  pb-5">
 
-                            <div class="flex flex-row w-full justify-between py-6 ">
-                                <h2 class="text-4xl font-extrabold dark:text-white h-14 flex-initial w-96 ">Curso:
-                                    {{ $courses->titulo }}
-                                </h2>
+                            <div class="flex flex-row w-full justify-between pl-8 ">
+                                <x-sub-title> Curso: {{ $courses->titulo }}</x-sub-title>
+                                <form method="POST" action="{{-- {{ route('inscricaoCurso.store') }} --}}" class="flex justify-end pt-4 pr-7">
+                                    @csrf
+                                    <input type="hidden" name="courseId" value="{{ $courses->id }}">
+                                    <button type="submit"
+                                        class="w-28  h-12 inline-flex items-center px-3 py-2 mx-2 text-lg text-center font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        Concluído
+                                    </button>
+                                </form>
                             </div>
                             <iframe class=" w-full aspect-video px-8 pb-6" src="{{ $courses->embed }}"
                                 title="{{ $courses->titulo }}" frameborder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 allowfullscreen></iframe>
-                            <a href="#"
-                                class="  inline-flex justify-center items-center  w-64  mx-2 text-2xl font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Próximo
-                                <svg aria-hidden="true" class="w-12 h-12 px-4  ml-2 -mr-1" fill="currentColor"
-                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                        clip-rule="evenodd"></path>
-                                </svg>
-                            </a>
                         </div>
 
 
                         {{-- Comentários --}}
-                        <div class="w-2/6 bg-gray-100 pl-2 flex flex-col justify-between pb-28" >
+                        <div class="w-2/6 bg-gray-100 pl-2 flex flex-col justify-between pb-28">
                             <ul class="">
                                 @forelse ($courses->comments as $comment)
                                     <li>
                                         <div class="bg-gray-100/50 border  rounded-sm">
-                                            <div>
-                                                <div class="flex flex-row items-center border-b border-black/50 pb-2 ">
-                                                    {{-- IMAGEMM --}}
-                                                    <div
-                                                        class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 border border-black/50">
-                                                        <svg class="absolute w-12 h-12 text-gray-400 -left-1"
+
+                                            <div class="flex flex-row items-center border-b border-black/50 pb-2 ">
+                                                {{-- IMAGEMM --}}
+                                                <div
+                                                    class="relative w-16 h-14 rounded-full overflow-hidden bg-gray-100  dark:bg-gray-600">
+
+                                                    @if ($comment->user)
+                                                        <img src="{{ url("storage/{$comment->user->image}") }}"
+                                                            alt="{{ $user->name }}"
+                                                            class="absolute w-14 h-14 rounded-full text-gray-400 -left-1">
+                                                    @else
+                                                        <svg class="absolute w-12 h-12 rounded-full text-gray-400 -left-1"
                                                             fill="currentColor" viewBox="0 0 20 20"
                                                             xmlns="http://www.w3.org/2000/svg">
                                                             <path fill-rule="evenodd"
                                                                 d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
                                                                 clip-rule="evenodd"></path>
                                                         </svg>
-
-                                                        {{-- @if ($user->image)
-                                                    <img src="{{ url("storage/{$user->image}") }}"
-                                                        alt="{{ $user->name }}" class="absolute w-12 h-12 text-gray-400 -left-1">
-                                                @else
-                                                <svg class="absolute w-12 h-12 text-gray-400 -left-1"
-                                                fill="currentColor" viewBox="0 0 20 20"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd"
-                                                    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                                                    clip-rule="evenodd"></path>
-                                            </svg>
-                                                @endif --}}
-
-
-                                                    </div>
-                                                    {{-- Nome do usuário e seu comentário --}}
-                                                    <div
-                                                        class="flex flex-col font-light text-gray-500 dark:text-gray-400 p-2  w-10/12">
-                                                        <p
-                                                            class="font-semibold text-gray-900 dark:text-white decoration-indigo-500 ">
-                                                            {{ $comment->user->name }} </p> {{ $comment->comment }}
-                                                    </div>
-
-                                                    {{-- Opção editar e deletar --}}
-                                                    @if (auth()->user()->id === $comment->user->id)
-                                                        <div class="pl-20 ">
-                                                            <x-dropdown align="right" width="48">
-                                                                <x-slot name="trigger">
-                                                                    <button
-                                                                        class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                                                        <div class="ml-1">
-                                                                            <svg class="w-6 h-6" aria-hidden="true"
-                                                                                fill="currentColor" viewBox="0 0 20 20"
-                                                                                xmlns="http://www.w3.org/2000/svg">
-                                                                                <path
-                                                                                    d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z">
-                                                                                </path>
-                                                                            </svg>
-                                                                        </div>
-                                                                    </button>
-                                                                </x-slot>
-
-                                                                <x-slot name="content">
-                                                                    <form
-                                                                        action="{{ route('comments.destroy', $comment->id) }}"
-                                                                        method="POST">
-                                                                        @csrf
-                                                                        @method('delete')
-                                                                        <button type="submit"
-                                                                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded w-full">Editar</button><br>
-                                                                        <button type="submit"
-                                                                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded w-full">Excluir</button>
-
-                                                                    </form>
-                                                                </x-slot>
-                                                            </x-dropdown>
-                                                        </div>
                                                     @endif
+
+
+                                                </div>
+                                                {{-- Nome do usuário e seu comentário --}}
+                                                <div
+                                                    class="flex flex-col font-light text-gray-500 dark:text-gray-400 p-2  w-10/12">
+                                                    <p
+                                                        class="font-semibold text-gray-900 dark:text-white decoration-indigo-500 ">
+                                                        {{ $comment->user->name }} </p> {{ $comment->comment }}
                                                 </div>
 
+                                                {{-- Opção editar e deletar --}}
+                                                @if ($user->id === $comment->user->id || auth()->user()->is_admin == 1)
+                                                    <div class="pl-20 ">
+                                                        <x-dropdown align="right" width="48">
+                                                            <x-slot name="trigger">
+                                                                <button
+                                                                    class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                                                    <div class="ml-1">
+                                                                        <svg class="w-6 h-6" aria-hidden="true"
+                                                                            fill="currentColor" viewBox="0 0 20 20"
+                                                                            xmlns="http://www.w3.org/2000/svg">
+                                                                            <path
+                                                                                d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z">
+                                                                            </path>
+                                                                        </svg>
+                                                                    </div>
+                                                                </button>
+                                                            </x-slot>
 
-                                            </div>
-                                            <div>
-                                                <p>Responder</p>
+                                                            <x-slot name="content">
+                                                                <form
+                                                                    action="{{ route('comments.destroy', $comment->id) }}"
+                                                                    method="POST">
+                                                                    @csrf
+                                                                    @method('delete')
+                                                                    <button type="submit"
+                                                                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded w-full">Editar</button><br>
+                                                                    <button type="submit"
+                                                                        class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded w-full">Excluir</button>
+
+                                                                </form>
+                                                            </x-slot>
+                                                        </x-dropdown>
+                                                    </div>
+                                                @endif
                                             </div>
                                         </div>
                                     </li>
@@ -171,7 +148,7 @@
                                     <div class="flex justify-end px-3 py-2 ">
                                         <button type="submit"
                                             class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-                                            Publicar postagem
+                                            Publicar comentário
                                         </button>
                                     </div>
                                 </form>
@@ -180,10 +157,6 @@
                     </div>
                 @endforeach
             </div>
-
         </div>
-
-        {{-- Comentário lateral --}}
-
     </section>
 </x-app-layout>
